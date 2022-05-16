c996c88db3bf9d9d5b8aa1714548a500f82bf167
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-16 15:40:44 -0600

    commit tree 78669b7d5b4a06606ba7710bdb8485f444c88634

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  let scuberGreeting
+  if (rideLength <= 400){
+    scuberGreeting = 'This one is on me!'
+    return scuberGreeting;
+  } 
+    else if ((rideLength >400) && (rideLength <=2000)){
+    scuberGreeting = 'That will be twenty bucks.'
+    return scuberGreeting;
+  } 
+    else if ((rideLength >2000) && (rideLength <2500)){
+      scuberGreeting = 'I will gladly take your thirty bucks.'
+      return scuberGreeting;
+  }
+    else return scuberGreeting = 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let scuberApproval
+  if (city === 'NYC'){
+    return scuberApproval = 'Ok, sounds good.';
+  } else {
+    return scuberApproval = 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm 
+  if (tip === 'generous'){
+    return charm = 'Thank you so much.';
+  }
+    else if (tip === 'not as generous'){
+      return charm = "Thank you."  ;
+    }
+      else return charm = 'Bye.';
 }
